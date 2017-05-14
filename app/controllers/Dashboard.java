f16787c7dbfb124c1da6d5889a251d95b6d5c8bd
Keelan Murphy <keelanmurphy90@gmail.com>
2017-05-14 22:44:18 +0100

    Member has the ability to add an assessment to their profile

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -12,9 +12,23 @@ public class Dashboard extends Controller
     public static void index()
     {
         Logger.info("Rendering Dashboard");
-    
+        
         Member member = Accounts.getLoggedInMember();
         List<Assessment> assessments = member.assessments;
         render("dashboard.html", member, assessments);
+    }
+    
+    public static void addAssessment(double weight,
+                                     double chest,
+                                     double thigh,
+                                     double upperArm,
+                                     double waist,
+                                     double hips) {
+        Member member = Accounts.getLoggedInMember();
+        Assessment newAssessment = new Assessment(weight, chest, thigh, upperArm, waist, hips, "");
+        member.assessments.add(newAssessment);
+        member.save();
+        Logger.info("Adding Assessment");
+        redirect("/dashboard");
     }
 }
