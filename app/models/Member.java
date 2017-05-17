6e76ceb3b3359e9a0e5e2692cd1a5ab80f7f7789
Keelan Murphy <keelanmurphy90@gmail.com>
2017-05-17 14:42:53 +0100

    Added statistics section and related java

--- app/models/Member.java
+++ app/models/Member.java
@@ -53,4 +53,137 @@ public class Member extends Model {
     {
         return this.password.equals(password);
     }
+    
+    /**
+     * This method returns a boolean to indicate if the member has an ideal body weight based on the Devine formula.<br>
+     *  For males, an ideal body weight is:     50 kg + 2.3 kg for each inch over 5 feet.<br>
+     *  For females, an ideal body weight is:   45.5 kg + 2.3 kg for each inch over 5 feet.<br>
+     *  <br>
+     *  Note: if no gender is specified, return the result of the female calculation
+     *
+     *  @return Returns true if the result of the devine formula is within 2 kgs (inclusive) of the starting weight; false if it is outside this range.
+     */
+    public String isIdealBodyWeight()
+    {
+        
+        
+        double genderWeight = 0;
+        
+        double heightInInches = convertHeightMetresToInches();
+        if (heightInInches < 60)
+        {
+            heightInInches = 60;
+        }
+        
+        if (gender.equals("Male"))
+        {
+            genderWeight = 50;
+        }
+        else
+        {
+            genderWeight = 45.5;
+        }
+        
+        double idealBodyWeight = genderWeight + ((heightInInches - 60) * 2.3);
+        if (startingWeight >= (idealBodyWeight - 7) && startingWeight <= (idealBodyWeight + 7)){
+            return "yellow";
+        }
+        else if (startingWeight >= (idealBodyWeight - 2) && startingWeight <= (idealBodyWeight + 2)) {
+            return "green";
+        }
+        else {
+            return "red";
+        }
+    }
+    
+    /**
+     * This method calculates the BMI for the member.
+     * @return the BMI value for the member. The number returned is truncated to two decimal places.
+     */
+    public double calculateBMI()
+    {
+        return toTwoDecimalPlaces((startingWeight / height) / height);
+    }
+    
+    /**
+     * This method determines the BMI category that the member belongs to.<br>
+     * <br>
+     * The category is determined by the magnitude of the members BMI according to the following:<br>
+     * <br>
+     *  BMI less than    15   (exclusive)                      is "VERY SEVERELY UNDERWEIGHT"<br>
+     *  BMI between      15   (inclusive) and 16   (exclusive) is "SEVERELY UNDERWEIGHT"<br>
+     *  BMI between      16   (inclusive) and 18.5 (exclusive) is "UNDERWEIGHT"<br>
+     *  BMI between      18.5 (inclusive) and 25   (exclusive) is "NORMAL"<br>
+     *  BMI between      25   (inclusive) and 30   (exclusive) is "OVERWEIGHT"<br>
+     *  BMI between      30   (inclusive) and 35   (exclusive) is "MODERATELY OBESE"<br>
+     *  BMI between      35   (inclusive) and 40   (exclusive) is "SEVERELY OBESE"<br>
+     *  BMI greater than 40   (inclusive)                      is "VERY SEVERELY OBESE"<br>
+     *
+     *  @return the BMI category that the member belongs to.
+     */
+    public String getBMICategory()
+    {
+        double bmi = calculateBMI();
+        if (bmi < 15)
+        {
+            return "VERY SEVERELY UNDERWEIGHT";
+        }
+        else if (bmi >= 15 && bmi < 16)
+        {
+            return "SEVERELY UNDERWEIGHT";
+        }
+        else if (bmi >= 16 && bmi < 18.5)
+        {
+            return "UNDERWEIGHT";
+        }
+        else if (bmi >= 18.5 && bmi < 25)
+        {
+            return "NORMAL";
+        }
+        else if (bmi >= 25 && bmi < 30)
+        {
+            return "OVERWEIGHT";
+        }
+        else if (bmi >= 30 && bmi < 35)
+        {
+            return "MODERATELY OBESE";
+        }
+        else if (bmi >= 35 && bmi < 40)
+        {
+            return "SEVERELY OBESE";
+        }
+        else if (bmi >= 40)
+        {
+            return "VERY SEVERELY OBESE";
+        }
+        
+        return "Error in BMI Calculation";
+    }
+    
+    public Assessment getLatestAssessment() {
+       if (assessments.size() != 0) {
+           return assessments.get(assessments.size());
+       }
+       return null;
+    }
+    
+    /**
+     * Returns the double sent as an argument truncated to two decimal places.
+     * @param num the number to be truncated to two decimal places
+     * @return the input truncated to two decimal places
+     */
+    private double toTwoDecimalPlaces(double num)
+    {
+        return (int) (num * 100 ) / 100.0;
+    }
+    
+    /**
+     * This method returns the member height converted from metres to inches.
+     * @return member height converted from metres to inches using the formula: metres multipled by 39.37. The number returned is truncated to two decimal places.
+     */
+    public double convertHeightMetresToInches()
+    {
+        double heightInInches = toTwoDecimalPlaces(height * 39.37);
+        return heightInInches;
+    }
 }
